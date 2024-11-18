913a090db0b51e376f1d1250a453bb9510c05d87
iksan nurpadillah <211210016@unmuhpnk.ac.id>
2024-11-19 00:42:05 +0700

    Update utils.js

--- src/utils.js
+++ src/utils.js
@@ -18,7 +18,6 @@ function displayHeader() {
     console.log('=================================================================='.cyan);
     console.log('=                       Ace Market                               ='.cyan);
     console.log('=                  Recode by vodka.ace                           ='.cyan);
-    console.log('= https://app.getgrass.io/register/?referralCode=3zoAM4QCy4c_086 ='.cyan);
     console.log('=================================================================='.cyan);
     console.log();
 }
